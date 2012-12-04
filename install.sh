5feb0f804ea690b0192d03c21793fff6d3290a76
Ubuntu <ubuntu@ip-10-62-26-211.ec2.internal>
2012-12-04 07:40:21 +0000

    Fixed xterm param in install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 ln -s ~/.vim/.vimrc ~/.vimrc
 
-LINE="export TERM='xterm-255color'"
+LINE="export TERM='xterm-256color'"
 
 if ! grep -Fx "$LINE" ~/.profile >/dev/null 2>/dev/null; then
     echo "Updating ~/.profile"
